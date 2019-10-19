bc342938211cf72769313d761a7ace16f5fcda97
Wilson <wilsnxs@38f9d34f0937.ant.amazon.com>
2019-10-19 16:00:40 -0700

    update boto info

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,20 @@
 import boto3
+import sys
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+def list_instances():
+    for i in ec2.instances.all():
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='shotty')
-    ec2 = session.resource('ec2')
-    
-    for i in ec2.instances.all():
-        print(i)
+    print(sys.argv)
+    list_instances()
